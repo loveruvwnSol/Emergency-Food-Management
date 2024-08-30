07657b25b2c4ba6db092e605001e11d4a14589be
1zll <maho-s125@ezweb.ne.jp>
2024-08-30 17:42:09 +0900

    create logo

---
+++ frontend/src/components/atoms/base/Logo.tsx
@@ -0,0 +1,16 @@
+import { Box, Image } from '@chakra-ui/react';
+import LogoIcon from '../../../images/icon-text.png';
+
+type LogoProps = {
+    size : string
+}
+
+const Logo : React.FC<LogoProps> =({ size = "md" }) => (
+    <Image
+        src={LogoIcon}
+        alt="備えの食卓"
+        boxSize={size}
+    />
+);
+
+export default Logo;
