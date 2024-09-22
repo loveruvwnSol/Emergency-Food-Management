cd665a74760b0dd6ae48422ace27227b6f583341
Nemoto99 <gaeul9717@gmail.com>
2024-09-23 06:10:08 +0900

    familyboard and familyboard moved into family.tsx

--- frontend/src/pages/Family.tsx
+++ frontend/src/pages/Family.tsx
@@ -1,10 +1,48 @@
 import { useState } from 'react';
-import FamilyBoard from '../components/templates/Family/FamilyBoard';
-import NoneFamily from '../components/organisms/Family/NoneFamily';
+import Header from '../components/organisms/base/Header';
+import { Box, Text } from '@chakra-ui/react';
+import SidebarItems from '../components/molecules/SidebarItems';
+import FamilyContents from '../components/templates/Family/FamilyContents';
+import InviteFamilyBtn from '../components/atoms/InviteFamilyBtn';
 
 const Family = () => {
   const [familyCheck, setFamilyCheck] = useState(true); //trueで家族ページfalseで家族招待ページ
-  return familyCheck ? <FamilyBoard /> : <NoneFamily />;
+  return (
+    <>
+      <Header />
+      {familyCheck ? (
+        <Box
+          display={'flex'}
+          alignItems={'start'}
+        >
+          <SidebarItems />
+          <FamilyContents />
+        </Box>
+      ) : (
+        <Box display={'flex'}>
+          <SidebarItems />
+          <Box
+            w={'1100px'}
+            h={'100vh'}
+            display={'flex'}
+            flexDirection={'column'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            pb={'60px'}
+          >
+            <Text
+              fontSize={'36px'}
+              fontWeight={'bold'}
+              mb={'48px'}
+            >
+              家族がいません
+            </Text>
+            <InviteFamilyBtn />
+          </Box>
+        </Box>
+      )}
+    </>
+  );
 };
 
 export default Family;
