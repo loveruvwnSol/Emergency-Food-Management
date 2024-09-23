1e0adee47d857e0e9ec25dc94fb777b6fd053a7a
sol <ocosio0907@gmail.com>
2024-09-23 22:06:05 +0900

    add homepage with widget

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,25 +1,25 @@
-import { useNavigate } from 'react-router-dom';
+import { Box } from "@chakra-ui/react";
+import Header from "../components/organisms/Base/Header";
+import SidebarItems from "../components/organisms/Base/SidebarItems";
+import { FoodItemList } from "../components/organisms/Base/FoodItemList";
+import { Widget } from "../components/molecules/Home/Widget";
 
 export const Home = () => {
-  const navigate = useNavigate();
   return (
-    <div>
-      <h1>HOME</h1>
-      <button
-        onClick={() => {
-          sessionStorage.removeItem('TOKEN_KEY');
-          navigate('/login');
-        }}
-      >
-        ログアウト
-      </button>
-      <button
-        onClick={() => {
-          navigate('/family');
-        }}
-      >
-        familypageへ
-      </button>
-    </div>
+    <Box>
+      <Header />
+      <Box display={"flex"}>
+        <SidebarItems />
+        <Box display={"flex"} flexDirection={"column"} w={"100%"}>
+          <Box display={"flex"} justifyContent={"space-between"}>
+            <FoodItemList title="非常食一覧" />
+            <Widget />
+          </Box>
+          <Box display={"flex"}>
+            <FoodItemList title="消費期限が1年以内の非常食" />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
