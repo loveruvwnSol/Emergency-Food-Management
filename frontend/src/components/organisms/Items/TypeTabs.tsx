e901bc658dbf641ecc838fc6637363e1e8025abe
Nemoto99 <gaeul9717@gmail.com>
2024-09-26 10:52:47 +0900

    edit tab name

--- frontend/src/components/organisms/Items/TypeTabs.tsx
+++ frontend/src/components/organisms/Items/TypeTabs.tsx
@@ -9,7 +9,7 @@ const TypeTabs = () => {
           w={'16%'}
           color={'#808080'}
         >
-          <Tab _selected={{ color: '#FB8B24' }}>全部</Tab>
+          <Tab _selected={{ color: '#FB8B24' }}>全て</Tab>
           <Tab _selected={{ color: '#FB8B24' }}>食料</Tab>
           <Tab _selected={{ color: '#FB8B24' }}>飲料</Tab>
         </TabList>
