114e7ac862651135d463c9d25210af3376be48cc
Nemoto99 <gaeul9717@gmail.com>
2024-09-26 11:15:42 +0900

    fix border bottom and tab size

--- frontend/src/components/organisms/Items/TypeTabs.tsx
+++ frontend/src/components/organisms/Items/TypeTabs.tsx
@@ -6,8 +6,13 @@ const TypeTabs = () => {
     <Box>
       <Tabs>
         <TabList
-          w={'16%'}
+          w={'210px'}
+          gap={2}
           color={'#808080'}
+          fontSize={'16px'}
+          borderWidth={'10px'}
+          border={'none'}
+          borderBottom={'1px solid #d7d7d7'}
         >
           <Tab _selected={{ color: '#FB8B24' }}>全て</Tab>
           <Tab _selected={{ color: '#FB8B24' }}>食料</Tab>
@@ -15,6 +20,7 @@ const TypeTabs = () => {
         </TabList>
         <TabIndicator
           mt='-1.5px'
+          w={'100%'}
           height='2px'
           bg='#FB8B24'
           borderRadius='1px'
