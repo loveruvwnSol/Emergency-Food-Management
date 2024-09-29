0045677ff83f56b4c5b0eb0da1f179f7a5222122
1zll <maho-s125@ezweb.ne.jp>
2024-09-30 00:15:15 +0900

    add InviteNotice page

---
+++ frontend/src/components/atoms/Notification/InviteBtn.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@chakra-ui/react';
+import { useState } from 'react';
+
+type InviteBtnProps = {
+    onClick?: () => void;
+};
+
+const InviteBtn: React.FC<InviteBtnProps> = ({ onClick }) => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(prev => !prev);
+        if (onClick) {
+            onClick();
+        }
+    };
+
+    return (
+        <Button
+            bgColor={isClicked ? "#828282" : "#FB8B24"}
+            color="#FFF"
+            w={24}
+            py={4}
+            borderRadius={50}
+            onClick={handleClick}
+            _hover={{ bgColor: isClicked ? "#828282" : "#FB8B24" }} // ホバー時の色変化はこのままでOK
+        >
+            承認
+        </Button>
+    );
+};
+
+export default InviteBtn;
