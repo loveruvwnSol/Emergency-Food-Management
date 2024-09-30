f9b25ceae7dec2c8e0814223fd737b12cd700367
Nemoto99 <gaeul9717@gmail.com>
2024-09-30 17:20:52 +0900

    length取得前に?でちぇっくするよう変更

--- frontend/src/components/organisms/Notification/ExpiryList.tsx
+++ frontend/src/components/organisms/Notification/ExpiryList.tsx
@@ -1,7 +1,7 @@
-import { Box, Text } from "@chakra-ui/react";
-import ExpiryItems from "../../molecules/Notification/ExpiryItems";
-import { useNotification } from "../../../hooks/notification";
-import { useEffect } from "react";
+import { Box, Text } from '@chakra-ui/react';
+import ExpiryItems from '../../molecules/Notification/ExpiryItems';
+import { useNotification } from '../../../hooks/notification';
+import { useEffect } from 'react';
 
 const ExpiryList = () => {
   const [{ notifications, UpdateReadStatus }] = useNotification();
@@ -11,22 +11,26 @@ const ExpiryList = () => {
   }, [notifications]);
 
   return (
-    <Box alignSelf="flex-start">
-      {notifications.length ? (
+    <Box alignSelf='flex-start'>
+      {notifications?.length ? (
         <>
           {notifications.map((e, idx) => (
-            <ExpiryItems key={idx} data={e.notification} items={e.items} />
+            <ExpiryItems
+              key={idx}
+              data={e.notification}
+              items={e.items}
+            />
           ))}
         </>
       ) : (
         <Text
           height={96}
-          w={"1000px"}
-          display={"flex"}
-          justifyContent={"center"}
-          alignItems={"center"}
+          w={'1000px'}
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
           fontSize={24}
-          fontWeight={"bold"}
+          fontWeight={'bold'}
         >
           通知はありません
         </Text>
--- frontend/src/components/templates/Notification/NotificationTabs.tsx
+++ frontend/src/components/templates/Notification/NotificationTabs.tsx
@@ -7,11 +7,11 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
-} from "@chakra-ui/react";
-import LowStock from "../../organisms/Notification/LowStock";
-import InviteNotice from "../../organisms/Notification/InviteNotice";
-import ExpiryList from "../../organisms/Notification/ExpiryList";
-import { useNotification } from "../../../hooks/notification";
+} from '@chakra-ui/react';
+import LowStock from '../../organisms/Notification/LowStock';
+import InviteNotice from '../../organisms/Notification/InviteNotice';
+import ExpiryList from '../../organisms/Notification/ExpiryList';
+import { useNotification } from '../../../hooks/notification';
 
 const NotificationTabs = () => {
   const [{ notifications, invitations }] = useNotification();
@@ -19,55 +19,64 @@ const NotificationTabs = () => {
   const unreadItems = notifications?.filter(
     (notification) => notification.notification.is_read === false
   );
-  
+
   return (
     <Box>
       <Tabs>
         <TabList
-          display={"flex"}
-          justifyContent={"space-between"}
-          w={"390px"}
+          display={'flex'}
+          justifyContent={'space-between'}
+          w={'390px'}
           gap={2}
-          color={"#808080"}
-          fontSize={"16px"}
-          borderWidth={"10px"}
-          border={"none"}
-          borderBottom={"1px solid #d7d7d7"}
+          color={'#808080'}
+          fontSize={'16px'}
+          borderWidth={'10px'}
+          border={'none'}
+          borderBottom={'1px solid #d7d7d7'}
         >
-          <Tab _selected={{ color: "#FB8B24" }} gap={3}>
+          <Tab
+            _selected={{ color: '#FB8B24' }}
+            gap={3}
+          >
             備蓄量不足
           </Tab>
-          <Tab _selected={{ color: "#FB8B24" }} gap={3}>
+          <Tab
+            _selected={{ color: '#FB8B24' }}
+            gap={3}
+          >
             消費期限間近
-            {unreadItems.length ? (
+            {unreadItems?.length ? (
               <Badge
-                backgroundColor="#FB8B24"
-                color="white"
-                borderRadius="full"
-                fontSize="0.8em"
+                backgroundColor='#FB8B24'
+                color='white'
+                borderRadius='full'
+                fontSize='0.8em'
                 w={5}
                 h={5}
-                textAlign={"center"}
+                textAlign={'center'}
               >
-                {unreadItems.length}{" "}
+                {unreadItems?.length}{' '}
               </Badge>
             ) : (
               <></>
             )}
           </Tab>
-          <Tab _selected={{ color: "#FB8B24" }} gap={3}>
+          <Tab
+            _selected={{ color: '#FB8B24' }}
+            gap={3}
+          >
             招待
-            {invitations.length ? (
+            {invitations?.length ? (
               <Badge
-                backgroundColor="#FB8B24"
-                color="white"
-                borderRadius="full"
-                fontSize="0.8em"
+                backgroundColor='#FB8B24'
+                color='white'
+                borderRadius='full'
+                fontSize='0.8em'
                 w={5}
                 h={5}
-                textAlign={"center"}
+                textAlign={'center'}
               >
-                {invitations.length}
+                {invitations?.length}
               </Badge>
             ) : (
               <></>
@@ -76,11 +85,11 @@ const NotificationTabs = () => {
         </TabList>
 
         <TabIndicator
-          mt="-1.5px"
-          w={"100%"}
-          height="2px"
-          bg="#FB8B24"
-          borderRadius="1px"
+          mt='-1.5px'
+          w={'100%'}
+          height='2px'
+          bg='#FB8B24'
+          borderRadius='1px'
         />
         <TabPanels>
           <TabPanel>
