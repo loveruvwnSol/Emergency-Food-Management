7da695b9c82ac3c802f864ee711b0c4d07ecba99
Nemoto99 <gaeul9717@gmail.com>
2024-09-30 16:37:30 +0900

    length前に? 追加

--- frontend/src/components/organisms/Base/Header.tsx
+++ frontend/src/components/organisms/Base/Header.tsx
@@ -1,8 +1,8 @@
-import Bell from "../../atoms/Base/Bell";
-import { Badge, Box } from "@chakra-ui/react";
-import Logo from "../../atoms/Base/Logo";
-import { useNotification } from "../../../hooks/notification";
-import { Link } from "react-router-dom";
+import Bell from '../../atoms/Base/Bell';
+import { Badge, Box } from '@chakra-ui/react';
+import Logo from '../../atoms/Base/Logo';
+import { useNotification } from '../../../hooks/notification';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [{ notifications, invitations }] = useNotification();
@@ -12,31 +12,47 @@ const Header = () => {
   );
 
   return (
-    <Box display={"flex"} justifyContent={"space-between"} mt={2} mb={12}>
-      <Box mt={2} ml={5}>
-        <Logo size="58" />
+    <Box
+      display={'flex'}
+      justifyContent={'space-between'}
+      mt={2}
+      mb={12}
+    >
+      <Box
+        mt={2}
+        ml={5}
+      >
+        <Logo size='58' />
       </Box>
-      <Link to="/notifications">
-        {unreadItems.length || invitations?.length ? (
-          <Box position="relative" display="inline-block" mr={3} mt={2}>
+      <Link to='/notifications'>
+        {unreadItems?.length || invitations?.length ? (
+          <Box
+            position='relative'
+            display='inline-block'
+            mr={3}
+            mt={2}
+          >
             <Bell />
             <Badge
-              position="absolute"
-              top="-2"
-              right="1"
-              backgroundColor="#FB8B24"
-              color="white"
-              borderRadius="full"
-              fontSize="0.8em"
+              position='absolute'
+              top='-2'
+              right='1'
+              backgroundColor='#FB8B24'
+              color='white'
+              borderRadius='full'
+              fontSize='0.8em'
               w={5}
               h={5}
-              textAlign={"center"}
+              textAlign={'center'}
             >
-              {unreadItems.length + invitations?.length}
+              {unreadItems?.length + invitations?.length}
             </Badge>
           </Box>
         ) : (
-          <Box mr={3} mt={2}>
+          <Box
+            mr={3}
+            mt={2}
+          >
             <Bell />
           </Box>
         )}
