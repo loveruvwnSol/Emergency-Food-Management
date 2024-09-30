c0b7f3ae93e8938507f9f2a098148f5577d26e79
sol <ocosio0907@gmail.com>
2024-09-30 12:59:13 +0900

    add notifications page routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Login } from './pages/Login';
-import { CreateAccount } from './pages/CreateAccount';
-import { useAuth } from './hooks/auth';
-import { useEffect } from 'react';
-import { Start } from './pages/Start';
-import Family from './pages/Family';
-import Settings from './pages/Settings';
-import Items from './pages/Items';
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { CreateAccount } from "./pages/CreateAccount";
+import { useAuth } from "./hooks/auth";
+import { useEffect } from "react";
+import { Start } from "./pages/Start";
+import Family from "./pages/Family";
+import Settings from "./pages/Settings";
+import Items from "./pages/Items";
+import Notifications from "./pages/Notifications";
 
 function App() {
   const [{ IsLoggedInUser }] = useAuth();
@@ -18,34 +19,14 @@ function App() {
 
   return (
     <Routes>
-      <Route
-        path='/'
-        element={<Home />}
-      />
-      <Route
-        path='/start'
-        element={<Start />}
-      />
-      <Route
-        path='/login'
-        element={<Login />}
-      />
-      <Route
-        path='/createAccount'
-        element={<CreateAccount />}
-      />
-      <Route
-        path='/family'
-        element={<Family />}
-      />
-      <Route
-        path='/items'
-        element={<Items />}
-      />
-      <Route
-        path='/settings'
-        element={<Settings />}
-      />
+      <Route path="/" element={<Home />} />
+      <Route path="/start" element={<Start />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/createAccount" element={<CreateAccount />} />
+      <Route path="/family" element={<Family />} />
+      <Route path="/items" element={<Items />} />
+      <Route path="/notifications" element={<Notifications />} />
+      <Route path="/settings" element={<Settings />} />
     </Routes>
   );
 }
--- frontend/src/components/organisms/Base/Header.tsx
+++ frontend/src/components/organisms/Base/Header.tsx
@@ -1,71 +1,46 @@
 import Bell from "../../atoms/Base/Bell";
-import { Alert, Badge, Box, Button } from "@chakra-ui/react";
+import { Badge, Box } from "@chakra-ui/react";
 import Logo from "../../atoms/Base/Logo";
 import { useNotification } from "../../../hooks/notification";
-import { useFamily } from "../../../hooks/family";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [{ notifications, invitations, setInvitations }] = useNotification();
-  const [{ JoinToFamily }] = useFamily();
+  const [{ notifications, invitations }] = useNotification();
 
-  console.log(notifications);
+  const unreadItems = notifications?.filter(
+    (notification) => notification.notification.is_read === false
+  );
 
   return (
     <Box display={"flex"} justifyContent={"space-between"} mt={2} mb={12}>
       <Box mt={2} ml={5}>
         <Logo size="58" />
       </Box>
-      {invitations.length !== 0 ? (
-        <Alert
-          w={"800px"}
-          bg={"#FB8B24"}
-          color={"white"}
-          fontWeight={"bold"}
-          display={"flex"}
-          justifyContent={"center"}
-          gap={10}
-          borderRadius={10}
-          boxShadow={"0 10px 10px #FED6B1"}
-          zIndex={10}
-        >
-          {invitations[0].Inviter.name}さんから招待が届きました！
-          <Button
-            bg={"white"}
-            color={"#FB8B24"}
-            onClick={() =>
-              JoinToFamily(invitations[0].family_id, setInvitations)
-            }
-          >
-            招待を受ける
-          </Button>
-        </Alert>
-      ) : (
-        <></>
-      )}
-
-      {notifications?.length || invitations?.length ? (
-        <Box position="relative" display="inline-block" mr={3} mt={2}>
-          <Bell />
-          <Badge
-            position="absolute"
-            top="-2"
-            right="1"
-            backgroundColor="#FB8B24"
-            color="white"
-            borderRadius="full"
-            fontSize="0.8em"
-            w={5}
-            h={5}
-            textAlign={"center"}
-          >
-            {notifications?.length + invitations?.length}
-          </Badge>
-        </Box>
-      ) : (
-        <Box mr={3} mt={2}>
-          <Bell />
-        </Box>
-      )}
+      <Link to="/notifications">
+        {unreadItems.length || invitations?.length ? (
+          <Box position="relative" display="inline-block" mr={3} mt={2}>
+            <Bell />
+            <Badge
+              position="absolute"
+              top="-2"
+              right="1"
+              backgroundColor="#FB8B24"
+              color="white"
+              borderRadius="full"
+              fontSize="0.8em"
+              w={5}
+              h={5}
+              textAlign={"center"}
+            >
+              {unreadItems.length + invitations?.length}
+            </Badge>
+          </Box>
+        ) : (
+          <Box mr={3} mt={2}>
+            <Bell />
+          </Box>
+        )}
+      </Link>
     </Box>
   );
 };
