1fdc2a8613a4c1d1278f3dd6bcacd57fac22fca4
sol <ocosio0907@gmail.com>
2024-10-15 10:50:25 +0900

    add get family stocks api

--- backend/app/model/stock.go
+++ backend/app/model/stock.go
@@ -6,3 +6,10 @@ type Stock struct {
 	Drink  int  `json:"drink" gorm:"not null"`
 	User   User `gorm:"foreignKey:UserID"` // Userとのリレーション
 }
+
+type FamilyStocks struct {
+	Goal               int `json:"goal"`
+	Current            int `json:"current"`
+	LongShelfLifeCount int `json:"longShelfLifeCount"`
+	NearExpiryCount    int `json:"nearExpiryCount"`
+}
