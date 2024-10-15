5f6a99570128582fce7a88c31abaa431b7910346
sol <ocosio0907@gmail.com>
2024-10-15 14:13:27 +0900

    add responsive and format code

--- frontend/src/components/molecules/Register/EmailInput.tsx
+++ frontend/src/components/molecules/Register/EmailInput.tsx
@@ -1,20 +1,24 @@
-import { FormControl, FormLabel, Input, InputGroup } from '@chakra-ui/react';
+import { FormControl, FormLabel, Input, InputGroup } from "@chakra-ui/react";
 
 type EmailInputProps = {
-    setEmail: React.Dispatch<React.SetStateAction<string>>
-}
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
+};
 
 const EmailInput: React.FC<EmailInputProps> = ({ setEmail }) => {
-
-    return (
-        <FormControl>
-            <FormLabel>メールアドレス</FormLabel>
-            <InputGroup size='md'>
-                <Input w={"600px"} type="email" placeholder="example@xxx.com" onChange={(e)=> setEmail(e.target.value)}
-                mb={10} />
-            </InputGroup>
-        </FormControl>
-    );
-}
+  return (
+    <FormControl>
+      <FormLabel>メールアドレス</FormLabel>
+      <InputGroup size="md" w={{ base: "350px", sm: "600px" }}>
+        <Input
+          w={"600px"}
+          type="email"
+          placeholder="example@xxx.com"
+          onChange={(e) => setEmail(e.target.value)}
+          mb={10}
+        />
+      </InputGroup>
+    </FormControl>
+  );
+};
 
 export default EmailInput;
--- frontend/src/components/molecules/Register/NameInput.tsx
+++ frontend/src/components/molecules/Register/NameInput.tsx
@@ -1,20 +1,24 @@
-import { FormControl, FormLabel, Input, InputGroup } from '@chakra-ui/react';
+import { FormControl, FormLabel, Input, InputGroup } from "@chakra-ui/react";
 
 type NameInputProps = {
-    setName: React.Dispatch<React.SetStateAction<string>>
-}
+  setName: React.Dispatch<React.SetStateAction<string>>;
+};
 
 const NameInput: React.FC<NameInputProps> = ({ setName }) => {
-
-    return (
-        <FormControl>
-            <FormLabel>名前</FormLabel>
-            <InputGroup size='md'>
-                <Input w={"600px"} type="text" placeholder="岩崎太郎" onChange={(e)=> setName(e.target.value)}
-                mb={10} />
-            </InputGroup>
-        </FormControl>
-    );
-}
+  return (
+    <FormControl>
+      <FormLabel>名前</FormLabel>
+      <InputGroup size="md" w={{ base: "350px", sm: "600px" }}>
+        <Input
+          w={"600px"}
+          type="text"
+          placeholder="岩崎太郎"
+          onChange={(e) => setName(e.target.value)}
+          mb={10}
+        />
+      </InputGroup>
+    </FormControl>
+  );
+};
 
 export default NameInput;
