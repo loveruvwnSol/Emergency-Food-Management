9e57dc4364abf2860ac910f771f690c4b5054fb4
sol <ocosio0907@gmail.com>
2024-10-16 17:13:09 +0900

    set upper limit

--- frontend/src/components/atoms/Notification/ProgressCircle.tsx
+++ frontend/src/components/atoms/Notification/ProgressCircle.tsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import { CircularProgress, CircularProgressLabel, Box } from '@chakra-ui/react';
+import React from "react";
+import { CircularProgress, CircularProgressLabel, Box } from "@chakra-ui/react";
 
 type ProgressCircleProps = {
-current: number;
-goal: number;
-color?: string;
+  current: number;
+  goal: number;
+  color?: string;
 };
 
-const ProgressCircle: React.FC<ProgressCircleProps> = ({ current, goal ,color}) => {
-    const percentage = goal > 0 ? Math.round((current / goal) * 100) : 0;
+const ProgressCircle: React.FC<ProgressCircleProps> = ({
+  current,
+  goal,
+  color,
+}) => {
+  const percentage =
+    goal > 0 ? Math.min(Math.round((current / goal) * 100), 100) : 0;
 
-    return (
+  return (
     <Box textAlign="center">
-        <CircularProgress value={percentage} color={color} size="300px" trackColor="#FFF" thickness="8px">
-            <CircularProgressLabel >
-                <Box display="flex" flexDirection="row" alignItems="center" justifyContent="center">
-                    <Box fontSize="64px" fontWeight="bold" color={color} pb={4} pr={2}>{percentage}</Box>
-                    <Box fontSize="40px">%</Box>
-                </Box>
-            </CircularProgressLabel>
-        </CircularProgress>
+      <CircularProgress
+        value={percentage}
+        color={color}
+        size="300px"
+        trackColor="#FFF"
+        thickness="8px"
+      >
+        <CircularProgressLabel>
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Box fontSize="64px" fontWeight="bold" color={color} pb={4} pr={2}>
+              {percentage}
+            </Box>
+            <Box fontSize="40px">%</Box>
+          </Box>
+        </CircularProgressLabel>
+      </CircularProgress>
     </Box>
-    );
-    };
+  );
+};
 
-    export default ProgressCircle;
+export default ProgressCircle;
