d88b009623d2113ba42d045b437568afb99110e4
sol <ocosio0907@gmail.com>
2024-10-24 12:54:04 +0900

    delete comment

--- frontend/src/components/molecules/Family/UserList.tsx
+++ frontend/src/components/molecules/Family/UserList.tsx
@@ -21,7 +21,7 @@ const UserList: React.FC<UserListProps> = ({ id, name, icon }) => {
       h={"60px"}
     >
       <Box display={"flex"} alignItems={"center"} gap={4}>
-        <Box //アイコン
+        <Box
           w={"55px"}
           h={"55px"}
           borderRadius={50}
