3094d87cc61214b32b48aec4384ff3146166ced0
sol <ocosio0907@gmail.com>
2024-10-24 12:55:12 +0900

    add validate for some models

--- backend/app/model/member.go
+++ backend/app/model/member.go
@@ -2,9 +2,9 @@ package model
 
 type Member struct {
 	Base
-	FamilyID int    `json:"family_id"`
-	UserID   int    `json:"user_id"`
+	FamilyID int    `json:"family_id" gorm:"not null" binding:"required"`
+	UserID   int    `json:"user_id" gorm:"not null" binding:"required"`
 	Role     string `json:"role" gorm:"not null"`
-	User     User   `gorm:"foreignKey:UserID"`   // Userとのリレーション
-	Family   Family `gorm:"foreignKey:FamilyID"` // Familyとのリレーション
+	User     User   `gorm:"foreignKey:UserID"`
+	Family   Family `gorm:"foreignKey:FamilyID"`
 }
